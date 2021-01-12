d6cd728cd12026c2cb951865acb5437e74c2eccd
samuel <atumasaake@gmail.com>
2021-01-11 23:51:09 -0800

    Included a linkedList example

--- euclid_bigo.py
+++ euclid_bigo.py
@@ -12,7 +12,25 @@ def Euclid_HCF(num1, num2):
 
 #Big O notation concept
 
-def Big_O(data, alphabet):
+data = [1, 2, 3]
+alphabet = ['A', 'B', 'C']
+
+def constant_time_Big_O(data):
+    last_data = data[len(data) - 1]
+    return f' This is a constant time because output, {last_data} \n takes the same amount of time to execute irrespective of the input'
+
+#print(constant_time_Big_O(data))
+
+def linear_Big_O(data):
+    big_o = 0
+    for datum in data:
+        big_o += 1
+        print(datum)
+    return f' Big o is linear here because {data} is executed \n {big_o} times which is equal to the number of inputs'
+
+#print(linear_Big_O(data))   
+
+def quadratic_Big_O(data, alphabet):
     bigo=len(data)
     total = 0
     for datum in data:
@@ -22,11 +40,113 @@ def Big_O(data, alphabet):
             print(datum, alpha)
     
     bigo2 = (total / bigo)
-    return f"Big_O is {bigo * bigo2} which is ({bigo} * {bigo2})"
+    return f"Big_O is {bigo * bigo2} which is ({bigo} * {bigo2}) i.e it is quadratic"
             
 
-data = [1, 2, 3]
-alphabet = ['A', 'B', 'C']
-#print(Big_O(data, alphabet))
+#print(quadratic_Big_O(data, alphabet))
+
+
+#Linked List walk through
+
+#The particular node class i.e the particular item
+class Node(object):
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+    def get_data(self):
+        return self.val
+
+    def set_data(self, val):
+        self.val = val
+    
+    def get_next(self):
+        return self.next
+
+    def set_next(self, next):
+        self.next = next
+
+# The List class as a whole (The linkedlist class)
+class LinkedList(object):
+    def __init__(self, head=None):
+        self.head = head
+        self.count = 0
+
+    def get_count(self):
+        return self.count
+    
+    #Add a node to the list
+    def insert(self, data):
+        #insert a new node at the head
+        new_node = Node(data)
+        
+        #set the current head to be the next value after the new node
+        new_node.set_next(self.head)
+        #set node as the new head of the linked list
+        self.head = new_node
+        #Increase list count by 1
+        self.count += 1
+        
+
+    def find(self, val):
+        #find the first item with a given value
+        item = self.head
+        while item is not None:
+            if item.get_data() == val:
+                return item
+            else:
+                item = item.get_next()
+            
+        return None
+
+    def deleteAt(self, idx):
+        #delete an item given the index
+        if idx > self.count - 1:
+            return
+        #If head, just set the next head as the new head
+        if idx == 0:
+            self.head = self.head.get_next()
+        else:
+            tempidx = 0
+            node = self.head
+
+            #Go to the linkednode just before the one we want to delete
+            #Done by looping through the linked list in this funny fashion
+            while tempidx < idx - 1:
+                node = node.get_next()
+                tempidx += 1
+            
+            #Now, point the previous node to the node after the node we want to delete
+            node.set_next(node.get_next().get_next())
+            self.count -= 1
+            
+
+
+    def dump_list(self):
+        #start at the head and print each value following
+        temporary_node = self.head
+
+        while temporary_node is not None:
+            print(f'Node: {temporary_node.get_data}')
+            temporary_node = temporary_node.get_next()
+
+
+#Create a linked list and insert some items
+itemList = LinkedList()
+itemList.insert(24)
+itemList.insert(31)
+itemList.insert(50)
+itemList.insert(18)
+itemList.dump_list()
+#Print out the list
+itemList.deleteAt(3)
+print("Item count: ", itemList.get_count())
+#print('finding item: ', itemList.find(24))
+#print('finding item: ', itemList.find(60))
+
+
+
+
 
 

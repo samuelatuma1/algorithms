2a2ca03aed9d4d2442b6ea1088c51c95224583ee
samuel <atumasaake@gmail.com>
2021-01-20 21:17:09 +0100

    Complete course on algorithm

--- euclid_bigo.py
+++ euclid_bigo.py
@@ -252,5 +252,197 @@ def factorial(num):
 
 #SOrting algorithms
 
+#Bubble sort
+list1 = [3, 7, 5, 1, 8, 2, 1, 23, 9, 17, 2]
+
+def bubble_sort(array):
+    for i in range(len(array)-1, 0, -1):
+        for j in range(i):
+            if array[j] > array[j+1]:
+                temp = array[j] 
+                array[j] = array[j+1]
+                array[j+1] = temp
+        
+        print('current state of array: ', array)
+
+#bubble_sort(list1)
 
 
+def mergesort(dataset):
+    if len(dataset) > 1:
+        mid = len(dataset) // 2
+        leftarr = dataset[:mid]
+        rightarr = dataset[mid:]
+
+        #recursively break down the array
+        mergesort(leftarr)
+        mergesort(rightarr)
+
+        #perform the indexing
+        i=0 #index into the left array
+        j=0 #index into the right array
+        k=0 #index into the array we are remerging
+
+        #while both contents have content
+        while i < len(leftarr) and j < len(rightarr):
+            if leftarr[i] < rightarr[j]:
+                dataset[k] = leftarr[i]
+                i += 1
+            else:
+                dataset[k] =rightarr[j]
+                j += 1
+            k += 1
+
+        #IF LEFT ARR still has residual elements
+        while i < len(leftarr):
+            dataset[k] = leftarr[i]
+            i += 1
+            k += 1
+        
+        #IF LEFT ARR still has residual elements
+        while j < len(rightarr):
+            dataset[k] = rightarr[j]
+            j += 1
+            k += 1
+    
+    return dataset
+
+        
+#print(mergesort([11, 3, 17, 1, 8, 2, 19, 7, 6, 5]))
+
+#QUICKSORT CODE EXAMPLE
+def quick_sort(sequence):
+    length = len(sequence)
+    #The break point
+    if length <= 1:
+        return sequence
+    
+    else:
+        #pop out the last item on the list. This will serve as the pivot
+        pivot = sequence.pop()
+    
+    #Create lists partitioning to hold the lower and higher values
+    items_lower = []
+    items_greater = []
+
+    #Now for every item left in the popped sequence
+    for item in sequence:
+        #If item is less than the pivot 
+        if item < pivot:
+            items_lower.append(item)
+        
+        else:
+            items_greater.append(item)
+
+    #Recurse 
+    #concatente, keeping the pivot between the lower and greater
+    return quick_sort(items_lower) + [pivot] + quick_sort(items_greater)
+
+
+#print(quick_sort([1,4, 2, 8, 1, 13, 2, 7]))
+
+
+#Module 5
+#SEARCHING DATA
+
+def search_unordered(item, itemslist):
+    for i in range(0, len(itemslist)):
+        if itemslist[i] == item:
+            return f'Search at position {i} '
+
+    
+    return None
+
+itemslist = [1,3,2, 5, 8, 11, 4, 17, 8, 8, 21, 2, 23]
+item = 8
+#print(search_unordered(item, itemslist))
+
+
+
+#Search ordered
+def binarysearch(item, array):
+    #listsize = 
+
+    lowerIdx = 0
+    upperIdx = len(array) - 1
+
+    #While there is still at least one value in the list
+    while lowerIdx <= upperIdx:
+        #find midpoint
+        midpoint = (lowerIdx + upperIdx) // 2
+
+        #If item is the middle, return the idx
+        if item == array[midpoint]:
+            return f'Item at index {midpoint}'
+
+        else:
+            #if item in lower part
+            if item < array[midpoint]:
+                upperIdx = midpoint - 1
+            
+            # else if item in upper half
+            elif item > array[midpoint]:
+                lowerIdx = midpoint + 1
+
+    return None
+        
+
+
+array = [1, 2, 3, 4, 6, 8, 9, 11, 20,  26]
+item = 11
+#print(binarysearch(item, array))
+
+
+#Determine if a list is osrted
+
+#Using the brute force method
+def is_sorted(array):
+    for index in range(0, len(array) - 1):
+        #Once the first unsorted value is reached, return false
+        if array[index] > array[index+1]:
+            return False
+        else: 
+            pass
+    
+    #At the end of the list, if no False has been reached, return True
+    return True
+
+
+#A pythonic apporach to solving is_sorted
+def is_sorted_pythonic(array):
+    return all(array[index] <= array[index+1] for index in range(0, len(array) - 1))
+
+#print(is_sorted(array))
+#print(is_sorted_pythonic(array))
+
+
+#Unique identifier
+def unique_identifier(array):
+    filter = {}
+    set_values = set()
+    for item in array:
+        if item in filter:
+            filter[item] += 1
+        else: 
+            filter[item] = 1
+
+        set_values.add(item)
+    
+    
+
+    return f'{set_values}, {filter}'
+
+#print(unique_identifier([1, 1, 2, 3, 5, 5, 5, 5, 7]))
+
+def find_max(array):
+    if len(array) == 1:
+        return array[0]
+    else:
+        op1 = array[0]
+        op2 = find_max(array[1:])
+        if op1 > op2:
+            return op1
+        else:
+            return op2
+    
+print(find_max([1, 1, 2, 3, 5, 5, 5, 5, 7]))
